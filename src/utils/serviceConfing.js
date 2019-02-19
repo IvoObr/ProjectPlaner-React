df79ff4ce89f9491e14ac1a7fa56d5059ac39042
IvoObr <ivo_obr@hotmail.com>
2019-02-19 16:01:11 +0200

    heroku url

--- src/utils/serviceConfing.js
+++ src/utils/serviceConfing.js
@@ -1,3 +1,1 @@
-export default (process.env.NODE_ENV === 'production' ? 'https://project-planer-node-js.herokuapp.com' : 'http://localhost:3002')
-
-console.log('**********PROCESS************',process.env.NODE_ENV)
+export default 'https://project-planer-node-js.herokuapp.com';
