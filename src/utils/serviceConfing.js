3aed35f831d4c487833c5a6f0f88980fcbaf9dbe
IvoObr <ivo_obr@hotmail.com>
2019-02-19 15:53:48 +0200

    logging process

--- src/utils/serviceConfing.js
+++ src/utils/serviceConfing.js
@@ -1,1 +1,3 @@
 export default (process.env.NODE_ENV === 'production' ? 'https://project-planer-node-js.herokuapp.com' : 'http://localhost:3002')
+
+console.log('**********PROCESS************',process.env.NODE_ENV)
