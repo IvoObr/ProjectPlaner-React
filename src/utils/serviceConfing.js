a1722f8fc929268fd2422cb6f4122a81313c04e8
IvoObr <ivo_obr@hotmail.com>
2019-03-14 16:34:47 +0200

    setting back-end url

--- src/utils/serviceConfing.js
+++ src/utils/serviceConfing.js
@@ -1,4 +1,1 @@
-console.log('*********************', process.env.EXPRESS_URL)
-const env = process.env.NODE_ENV || 'development';
-export default (env === 'production' && process.env.EXPRESS_URL) ?
-    process.env.EXPRESS_URL : 'https://project-planer-node-js.herokuapp.com';
+export default 'https://project-planer-node-js.herokuapp.com';
