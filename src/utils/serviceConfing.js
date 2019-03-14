501c0b517046b336e8cc24c7fbe14ba9681a4fae
IvoObr <ivo_obr@hotmail.com>
2019-03-14 15:42:47 +0200

    console.log

--- src/utils/serviceConfing.js
+++ src/utils/serviceConfing.js
@@ -1,5 +1,4 @@
-console.log('*********************', process)
-console.table(process);
+console.log('*********************', process.env.EXPRESS_URL)
 const env = process.env.NODE_ENV || 'development';
 export default (env === 'production' && process.env.EXPRESS_URL) ?
     process.env.EXPRESS_URL : 'https://project-planer-node-js.herokuapp.com';
